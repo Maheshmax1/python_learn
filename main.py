e2833a93fb759e1625131186d4b14bab9a4e4165
Maheshmax1 <maheshmaari2409@gmail.com>
2025-10-08 14:37:32 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# Tara and Jyoti were given one number each.
+# She has to find whether the sum of the two numbers
+# is divisible by 5 or not . If yes then print 1 else print 0
+
+
+
+tara=int(input("Enter your number"))
+jyothi=int(input("Enter your number"))
+
+print(tara,jyothi)
+
+add=(tara+jyothi)
+print(add)
+
+if add%5 == 0 :
+    print(1)
+else:
+    print(0)
+     
+
+
+
+
---
+++ sum_2/main.py
@@ -0,0 +1,9 @@
+# Given a number N, print yes if the number is a multiple of 7 else print no.
+
+n=int(input("enter your number"))
+print(n)
+
+if n%7 ==0:
+    print("yes")
+else:
+    print("no")    
